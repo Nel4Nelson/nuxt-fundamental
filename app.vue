435548e25ffa70ec48c2b34d4c4e305be820b719
Ekwueme Nelson <an.ekwueme@stu.unizik.edu.ng>
2023-02-16 08:06:14 +0000

    initial commit

---
+++ app.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Photo Gallery with Nuxt</h1>
+    <button @click="viewPhotos">View photos</button>
+    <ul>
+      <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+        <img :src="photo.thumbnailUrl" alt="">
+        <!-- <p>{{ photo.thumbnailUrl }}</p> -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import { defineNuxtComponent } from '#app';
+
+export default defineNuxtComponent ({
+  data: () => ({
+    photoGallery: []
+  }),
+  methods: {
+    viewPhotos() {
+      fetch('https://jsonplaceholder.typicode.com/photos/')
+        .then(response => response.json())
+        .then(json => {
+          this.photoGallery = json;
+        })
+    }
+  }
+
+})
+
+</script>
